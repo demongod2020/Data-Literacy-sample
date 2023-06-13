b98a296b0b42b487da83a21b5dca21403948bc46
skypunish <jiho.etri@gmail.com>
2023-06-13 19:03:00 +0900

    분산, 표준편차 평균 계산기

--- testmine.c
+++ testmine.c
@@ -1,25 +1,49 @@
 #include <stdio.h>
+#include <math.h>
+
+#define MAX_SIZE 100
+
+float calculateMean(int arr[], int size) {
+    int sum = 0;
+    int i;
+    for (i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return (float)sum / size;
+}
+
+float calculateVariance(int arr[], int size, float mean) {
+    float variance = 0;
+    int i;
+    for (i = 0; i < size; i++) {
+        variance += pow(arr[i] - mean, 2);
+    }
+    return variance / size;
+}
+
+float calculateStandardDeviation(float variance) {
+    return sqrt(variance);
+}
 
 int main() {
-    int n, i;
-    float sum = 0, average;
+    int numbers[MAX_SIZE];
+    int size, i;
 
     printf("Please enter the number of digits: ");
-    scanf("%d", &n);
+    scanf("%d", &size);
 
-    int numbers[n];
-
-    printf("%d Enter  numbers:\n", n);
-    for (i = 0; i < n; i++) {
+    printf("Please enter the number of %d:\n", size);
+    for (i = 0; i < size; i++) {
         scanf("%d", &numbers[i]);
-        sum += numbers[i];
     }
 
-    average = sum / n;
+    float mean = calculateMean(numbers, size);
+    float variance = calculateVariance(numbers, size, mean);
+    float stdDeviation = calculateStandardDeviation(variance);
 
-    printf("average: %.2f\n", average);
+    printf("average: %.2f\n", mean);
+    printf("Dispersion: %.2f\n", variance);
+    printf("Standard Deviation: %.2f\n", stdDeviation);
 
     return 0;
 }
-
-
